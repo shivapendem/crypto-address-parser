f723fd4e070cff25f30f34bda7603ba6ffea730c
shivapendem <pendem.shiva89@gmail.com>
2024-05-28 16:44:40 +0530

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# crypto-address-parser
+# Description
+    npm package for crypto address parser.
+# Installation
+    `npm i crypto-address-parser --save`
+        
+    let cpr = require('crypto-address-parser');
+
+# How to use
+Let's see about js code:
+```js
+    let cpr = require('crypto-address-parser');
+    
+    let _addressinfo=cpr.parseaddress("ethereum:0xe8312ec868303fc3f14DeA8C63A1013608038801@0x39?amount=0.003985&contract=0x");
+    console.log(_addressinfo);
+
+    ```
+    
+# Support
+
+Happy to add more, and need any updates, do get in touch on my telegram over [@chigovera](https://t.me/chigovera)
---
+++ index.js
@@ -0,0 +1,41 @@
+var querystring = require('querystring');
+function parseaddress(inputString) {
+	let data={};
+	let addr=inputString;
+	if(inputString.indexOf("?")>0)
+	{
+		data.params=JSON.parse(JSON.stringify(querystring.parse(inputString.substring(inputString.indexOf("?")+1))));
+		addr=inputString.substring(0,inputString.indexOf("?"));
+	}
+	if(addr.indexOf("@")>0)
+	{
+		if(addr.split("@")[0].indexOf(":")>0)
+		{
+			data.address=addr.split("@")[0].split(":")[1];
+			data.chain=addr.split("@")[0].split(":")[0];
+		}
+		else
+		{
+			data.address=addr.split("@")[0];
+		}
+		data.network=addr.split("@")[1];
+	}
+	else
+	{
+		if(addr.indexOf(":")>0)
+		{
+			data.address=addr.split(":")[0];
+			data.chain=addr.split(":")[1];
+		}
+		else
+		{
+			data.address=addr;
+		}
+	}
+	//console.log(data);
+	return data;
+};
+
+module.exports = {
+	parseaddress
+}
